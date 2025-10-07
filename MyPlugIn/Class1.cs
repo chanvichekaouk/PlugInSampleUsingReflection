a26c90a42f3fad2926785b4b8302d9f38340e269
Vic <chanvicheka.ouk@gmail.com>
2025-10-07 19:23:18 -0400

    Checking in our PlugIn called MyPlugIn which we will use later in our main console app!

---
+++ MyPlugIn/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPlugIn
+{
+    public class Greeting
+    {
+        public string Greet(string name)
+        {
+            return $"Hello {name}! It is nice meeting you. This message is sent from the PlugIn...";
+        }
+    }
+}
